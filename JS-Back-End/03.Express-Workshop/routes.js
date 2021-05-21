0c33e6238955d15f000c77005b217851d74607ff
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-21 12:19:29 +0300

    Added 404 page handler if no existing endpoint

--- JS-Back-End/03.Express-Workshop/routes.js
+++ JS-Back-End/03.Express-Workshop/routes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
 
 const productController = require('./controllers/productController');
+const aboutController = require('./controllers/aboutController.js');
 
 const router = Router();
 
 router.use('/', productController);
+router.use('/about', aboutController);
+router.get('*', ( req, res ) => {
+    res.render('404', { layout: false });
+})
 
 module.exports = router;
