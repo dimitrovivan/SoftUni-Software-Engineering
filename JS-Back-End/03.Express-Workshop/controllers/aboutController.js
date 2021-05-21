5d992920df131c0a4f06ed95504ba9f959bedec5
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-21 12:25:09 +0300

    Add main layout and refactored all views

--- JS-Back-End/03.Express-Workshop/controllers/aboutController.js
+++ JS-Back-End/03.Express-Workshop/controllers/aboutController.js
@@ -3,6 +3,6 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', ( req, res ) => {
-    res.render('about', {layout: false});
+    res.render('about');
 })
 module.exports = router;
