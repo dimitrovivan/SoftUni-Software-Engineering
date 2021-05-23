b94dc1f58421c48bba05a76273c1d95b20141209
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-23 19:19:45 +0300

    Added views and main layout set up

--- JS-Back-End/Cubicle/index.js
+++ JS-Back-End/Cubicle/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const config = require('./config/config');
+const handlebars = require('express-handlebars');
+
 const app = express();
+
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 const PORT = config.PORT;
 
