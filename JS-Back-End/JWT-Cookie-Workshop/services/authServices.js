b8cc61532df7ec3683bc2cb6422c9842a82fd49d
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-28 11:22:49 +0300

    Fixed register bug when username is taken

--- JS-Back-End/JWT-Cookie-Workshop/services/authServices.js
+++ JS-Back-End/JWT-Cookie-Workshop/services/authServices.js
@@ -3,14 +3,11 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const {SECRET} = require('../config/auth');
 
-async function getUserByName(username) {
-      let result = await User.findOne({username});
+const getUserByName = (username) => User.findOne({username});
 
-      if(result) return result;
-      return null;
-}
 async function register(username, password) {
-    if(!getUserByName(username)) throw {message: "Username taken"};
+    let isExisting = await getUserByName(username);
+    if (isExisting) throw {message: "Username taken"};
 
     let hashedPassword = await bcrypt.hash(password, 5);
     let user = new User({username, password: hashedPassword});
