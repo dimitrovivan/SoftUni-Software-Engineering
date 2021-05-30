2eb23080e3ce690376ce4459c7ffeffa0ea92d74
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-30 12:58:17 +0300

    Added hashing functionality

--- JS-Back-End/Video-Tutorials/config/auth.js
+++ JS-Back-End/Video-Tutorials/config/auth.js
@@ -1,4 +1,5 @@
 module.exports = {
     SECRET: 'asopgfo1p2ikgp1gkjwaasiovjaosigiowqcoasasjgpasjgo1j21ju4312u3129039129312939123ewqfuaha',
-    USER_COOKIE: 'user-session'
+    USER_COOKIE: 'user-session',
+    HASH_SALT: 7
 }
--- JS-Back-End/Video-Tutorials/services/authServices.js
+++ JS-Back-End/Video-Tutorials/services/authServices.js
@@ -1,21 +1,25 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
-const authConfig = require('../config/auth');
+const {SECRET, HASH_SALT} = require('../config/auth');
 
-async function register(data) {
-    let isAvaiableUsername = await User.findOne({username: data.username});
+async function register({username, password}) {
+    let isAvaiableUsername = await User.findOne({username});
     if(isAvaiableUsername) throw {message: "Username is already taken"};
 
-    let user = User(data);
+    let hashedPassword = await bcrypt.hash(password, HASH_SALT)
+    let user = User({username, password: hashedPassword});
     return user.save();
 }
 
-async function login(data) {
-    let user = await User.findOne({username: data.username});
+async function login({username, password}) {
+    let user = await User.findOne({username});
     if(!user) throw {message: "User doesn't exists"};
-    if(data.password !== user.password) throw {message: "Invalid password"};
 
-    return jwt.sign({_id:user._id}, authConfig.SECRET);
+    let isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) throw {message: "Invalid password"};
+    
+    return jwt.sign({_id:user._id}, SECRET);
 }
 
 module.exports = {
