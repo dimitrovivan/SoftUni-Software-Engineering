6dbd644eaa8c5fdcfbecb70f7d6969784e377078
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-30 10:29:57 +0300

    Added views

--- JS-Back-End/Video-Tutorials/app.js
+++ JS-Back-End/Video-Tutorials/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { PORT } = require('./config/config');
 
+const router = require('./router');
 const app = express();
+
+require('./config/express')(app);
+
+app.use(router);
 
 app.listen(PORT, console.log.bind(console, `Server is listenning on port ${PORT}`))
