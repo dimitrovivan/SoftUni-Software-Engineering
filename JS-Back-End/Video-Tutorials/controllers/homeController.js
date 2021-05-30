acc6798e0f3d6e3e0681626cec10c05750f8a8ec
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-30 15:19:54 +0300

    Updated home view

--- JS-Back-End/Video-Tutorials/controllers/homeController.js
+++ JS-Back-End/Video-Tutorials/controllers/homeController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('guest-home');
+    res.render('home');
 })
 
 module.exports = router;
--- JS-Back-End/Video-Tutorials/middlewares/auth.js
+++ JS-Back-End/Video-Tutorials/middlewares/auth.js
@@ -9,7 +9,7 @@ function checkForUserStateBeforeRequest(req, res, next) {
     if(!cookie) return next();
 
     jwt.verify(cookie[1], SECRET, (err, decoded) => {
-        if(err) return res.render('guest-home', {message: 'Do not try this again, you may be banned! Reason: false cookie info'});
+        if(err) return res.render('home', {message: 'Do not try this again, you may be banned! Reason: false cookie info'});
 
         User.findById(decoded._id).lean()
              .then(user => {
@@ -26,7 +26,7 @@ function isAuth(check = 'authentication') {
     if(!cookie) return check == 'authorization' ? res.redirect('login') : next();
 
     jwt.verify(cookie[1], SECRET, (err, decoded) => {
-        if(err) return res.render('guest-home', {message: 'Do not try this again, you may be banned! Reason: false cookie info'});
+        if(err) return res.render('home', {message: 'Do not try this again, you may be banned! Reason: false cookie info'});
         
         return check == 'authentication' ? res.redirect('/') : next();
     });
