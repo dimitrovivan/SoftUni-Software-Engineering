adda8fca02cc8a9d26df1c01baf37ea6f1c44854
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-05-30 10:40:48 +0300

    Added controllers
    Sucessfully rendering login register home view on GET request

--- JS-Back-End/Video-Tutorials/router.js
+++ JS-Back-End/Video-Tutorials/router.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
 
-router.get('/', (req, res) => {
-    res.render('login')
-})
+router.use('/', homeController); 
+router.use('/auth', authController); 
 
 module.exports = router;
 
