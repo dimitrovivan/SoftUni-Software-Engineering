2ada0e391ac8217d21a64007304f304cb9420511
Ivan Dimitrov <bgivandimitrov@gmail.com>
2021-07-01 12:13:48 +0300

    Added main component

--- REACT-JS/03-Origami-Workshop/client/src/App.js
+++ REACT-JS/03-Origami-Workshop/client/src/App.js
@@ -1,10 +1,12 @@
 import style from './App.module.css';
 import Header from './components/Header';
+import Main from './components/Main'
 
 function App() {
   return (
     <div className={style.app}>
         <Header />
+        <Main />
     </div>
   );
 }
---
+++ REACT-JS/03-Origami-Workshop/client/src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import style from './Main.module.css';
+
+function Main() {
+    return (
+        <main className={style.main}>
+            <h1 className={style.mainHeading}>Sooooooooooome heading</h1> 
+        </main>
+    )
+}
+
+
+export default Main;
