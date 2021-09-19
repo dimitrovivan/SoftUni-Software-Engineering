757b84d3bc3fab5600b0864ef2cc25a655baea75
dimitrovivan <bgivandimitrov@gmail.com>
2021-09-19 12:02:32 +0300

    Added footer component

---
+++ qua-test/secondtask/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className="m-footer">
+            <h5 className="copyright-text"> © 2021 Bike Rent Company. All rights reserverd.</h5>
+        </footer>
+    )
+}
+
+export default Footer;
