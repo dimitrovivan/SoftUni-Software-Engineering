fcbb39ead4de961cbdcaec69d39da31b081eef28
dimitrovivan <bgivandimitrov@gmail.com>
2021-10-01 12:31:21 +0300

    Re-add notification component

---
+++ React-Revision/routing/client/src/components/Notifications/Notifications.js
@@ -0,0 +1,19 @@
+import './Notifications.css';
+
+const Notifications = () => {
+    return (
+        <div id="notifications">
+        <div id="loadingBox" class="notification">
+          <span>Loading …</span>
+        </div>
+        <div id="infoBox" class="notification">
+          <span>Info</span>
+        </div>
+        <div id="errorBox" class="notification">
+          <span>Error</span>
+        </div>
+      </div>
+    )
+}
+
+export default Notifications;
