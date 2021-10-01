1b3b7ed06bc3eba9a434fa1bfd58eb92162dd730
dimitrovivan <bgivandimitrov@gmail.com>
2021-10-01 12:29:06 +0300

    Initial commit

---
+++ React-Revision/routing/client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main'
+
+function App() {
+  return (
+    <>
+      <div id="container">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
