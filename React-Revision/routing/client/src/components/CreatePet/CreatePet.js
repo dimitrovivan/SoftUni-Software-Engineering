5a9e89485c35c556f768e39bb5afe915114140ae
dimitrovivan <bgivandimitrov@gmail.com>
2021-10-01 12:30:26 +0300

    Add create pet

---
+++ React-Revision/routing/client/src/components/CreatePet/CreatePet.js
@@ -0,0 +1,49 @@
+const CreatePet = () => {
+    return (
+        <section className="create">
+            <form action="#" method="post">
+                <fieldset>
+                    <legend>Add new Pet</legend>
+                    <p className="field">
+                        <label for="name">Name</label>
+                        <span className="input">
+                            <input type="text" name="name" id="name" placeholder="Name" />
+                            <span className="actions"></span>
+                        </span>
+                    </p>
+                    <p className="field">
+                        <label for="description">Description</label>
+                        <span className="input">
+                            <textarea rows="4" cols="45" type="text" name="description" id="description"
+                                placeholder="Description"></textarea>
+                            <span className="actions"></span>
+                        </span>
+                    </p>
+                    <p className="field">
+                        <label for="image">Image</label>
+                        <span className="input">
+                            <input type="text" name="imageURL" id="image" placeholder="Image" />
+                            <span className="actions"></span>
+                        </span>
+                    </p>
+                    <p className="field">
+                        <label for="category">Category</label>
+                        <span className="input">
+                            <select type="text" name="category">
+                                <option>Cat</option>
+                                <option>Dog</option>
+                                <option>Parrot</option>
+                                <option>Reptile</option>
+                                <option>Other</option>
+                            </select>
+                            <span className="actions"></span>
+                        </span>
+                    </p>
+                    <input className="button" type="submit" className="submit" value="Add Pet" />
+                </fieldset>
+            </form>
+        </section>
+    )
+}
+
+export default CreatePet;
