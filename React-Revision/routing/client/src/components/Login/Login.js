e91a0f792cdea81af2b50647215f4eaafac0b240
dimitrovivan <bgivandimitrov@gmail.com>
2021-10-01 12:30:12 +0300

    Add login and register

---
+++ React-Revision/routing/client/src/components/Login/Login.js
@@ -0,0 +1,30 @@
+const Login = () => {
+    return (
+        <section className="login">
+            <form action="#" method="post">
+                <fieldset>
+                    <legend>Login</legend>
+                    <p className="field">
+                        <label for="username">Username</label>
+                        <span className="input">
+                            <input type="text" name="username" id="username" placeholder="Username" />
+                            <span className="actions"></span>
+                            <i className="fas fa-user"></i>
+                        </span>
+                    </p>
+                    <p className="field">
+                        <label for="password">Password</label>
+                        <span className="input">
+                            <input type="password" name="password" id="password" placeholder="Password" />
+                            <span className="actions"></span>
+                            <i className="fas fa-key"></i>
+                        </span>
+                    </p>
+                    <input className="button" type="submit" className="submit" value="Login" />
+                </fieldset>
+            </form>
+        </section>
+    )
+}
+
+export default Login;
