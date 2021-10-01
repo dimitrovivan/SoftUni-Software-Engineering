a2476ceb47f1037ae0043dc55a8d100a56838e86
dimitrovivan <bgivandimitrov@gmail.com>
2021-10-01 12:29:26 +0300

    Add header component

---
+++ React-Revision/routing/client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import Navigation from './Navigation/Navigation';
+
+const Header = () => {
+    return (
+        <header id="site-header">
+          <Navigation />
+        </header>
+    )
+}
+
+export default Header;
